80b929532bf1ab0eea206175b8e9751834cf5d6a
Bas van Driel <basvandriel94@gmail.com>
2014-12-27 14:24:46 +0100

    Added PHP documentation and moved all files in folders

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <script src="data/js-files/data_personen_auto.js" type="application/javascript"></script>
+    <script src="js/utility.js" type="application/javascript"></script>
+    <script src="js/road_tax_data_mapper.js" type="application/javascript"></script>
+    <script src="js/data_parser.js" type="application/javascript"></script>
+</head>
+<body>
 <?php
 
+    require "vehicle/Car.php";
+    require "vehicle/owner/CarOwner.php";
+    require "vehicle/calculator/RunningCostCalculator.php";
+    require "vehicle/calculator/impl/CarRunningCostCalculator.php";
+    require "vehicle/FuelType.php";
+    require "vehicle/Province.php";
+
     $myCar    = new Car("BMW epic", 1999, FuelType::BENZINE, 209, 900.99);
-    $carOwner = new CarOwner("Bas van Driel", Province::ZEELAND);
+    $carOwner = new CarOwner($myCar, Province::ZEELAND);
     $runningCostsCalculator = new CarRunningCostCalculator($myCar, $carOwner);
 
     echo $runningCostsCalculator->calculateFuelPrice();
+?>
+</body>
+</html>
+
---
+++ vehicle/Province.php
@@ -0,0 +1,68 @@
+<?php
+
+    /**
+     * This class provides all the provinces in constants within integer format
+     */
+    class Province
+    {
+
+        /**
+         * Defines the province Noord Holland
+         */
+        const NOORD_HOLLAND = 0;
+
+        /**
+         * Defines the province Utrecht
+         */
+        const UTRECHT = 1;
+
+        /**
+         * Defines the province Noord Brabant
+         */
+        const NOORD_BRABANT = 2;
+
+        /**
+         * Defines the province Limburg
+         */
+        const LIMBURG = 4;
+
+        /**
+         * Defines the province Flevoland
+         */
+        const FLEVOLAND = 8;
+
+        /**
+         * Defines the province Zeeland
+         */
+        const ZEELAND = 16;
+
+        /**
+         * Defines the province Overijssel
+         */
+        const OVERIJSSEL = 32;
+
+        /**
+         * Defines the province Groningen
+         */
+        const GRONINGEN = 64;
+
+        /**
+         * Defines the province Gelderland
+         */
+        const GELDERLAND = 128;
+
+        /**
+         * Defines the province Drenthe
+         */
+        const DRENTHE = 256;
+
+        /**
+         * Defines the province Friesland
+         */
+        const FRIESLAND = 512;
+
+        /**
+         * Defines the province Zuid Holland
+         */
+        const ZUID_HOLLAND = 1024;
+    }
